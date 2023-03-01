37b50c6637cf00ea2ca80b7672dd45189d2fa4d1
Adadezer <adadezer@gmail.com>
2023-03-01 10:10:52 -0300

    feat: :sparkles: dados e seleção de elementos

--- jsvanilla/javascript/script.js
+++ jsvanilla/javascript/script.js
@@ -1,1 +1,53 @@
-console.log('teste');
+// IMC DATA
+const data = [
+  {
+    min: 0,
+    max: 18.4,
+    classification: "Menor que 18,5",
+    info: "Magreza",
+    obesity: "0",
+  },
+  {
+    min: 18.5,
+    max: 24.9,
+    classification: "Entre 18,5 e 24,9",
+    info: "Normal",
+    obesity: "0",
+  },
+  {
+    min: 25,
+    max: 29.9,
+    classification: "Entre 25,0 e 29,9",
+    info: "Sobrepeso",
+    obesity: "I",
+  },
+  {
+    min: 30,
+    max: 39.9,
+    classification: "Entre 30,0 e 39,9",
+    info: "Obesidade",
+    obesity: "II",
+  },
+  {
+    min: 40,
+    max: 99,
+    classification: "Maior que 40,0",
+    info: "Obesidade grave",
+    obesity: "III",
+  },
+];
+
+// Seleção de elementos
+
+const imcTable = document.querySelector('#imc-table');
+
+const heightInput = document.querySelector('#height');
+const wightInput = document.querySelector('#weight');
+const calcBtn = document.querySelector('#calc-btn');
+const clearBtn = document.querySelector('#clear-btn');
+
+// Funções
+
+// Inicialização
+
+// Eventos
